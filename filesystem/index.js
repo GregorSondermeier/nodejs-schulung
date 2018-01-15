a657c1cc309fa1b2fefbf983a9d107dd0025ae9d
Gregor Sondermeier (Funke Digital) <g.sondermeier@funkedigital.de>
2018-01-15 17:09:01 +0100

    ex. 3 - folder copy wip

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,4 +1,5 @@
-const fs = require('fs');
+const fs = require('fs'),
+	path = require('path');
 
 function copyFile(source, target, cb) {
 	let cbCalled = false;
@@ -32,4 +33,61 @@ function copyFile(source, target, cb) {
 	}
 }
 
-copyFile('./filesystem/src.txt', './filesystem/target.txt');
+function copyFolder(source, target) {
+	let files = [];
+
+	//check if folder needs to be created or integrated
+	function createTargetIfNotExists(target, cb) {
+		fs.open(target, 'r', (err, fd) => {
+			if (err) {
+				if (err.code === 'ENOENT') {
+					fs.mkdir(target, (err) => {
+						if (err) {
+							console.error(err);
+						} else {
+							cb(target);
+						}
+					});
+				}
+			} else {
+				cb(target);
+			}
+		});
+	}
+
+	createTargetIfNotExists(target, (existingTarget) => {
+
+	});
+
+	/*
+	//copy
+	if ( fs.lstatSync( source ).isDirectory() ) {
+		files = fs.readdirSync( source );
+		files.forEach( function ( file ) {
+			let curSource = path.join( source, file );
+			if ( fs.lstatSync( curSource ).isDirectory() ) {
+				copyFolder( curSource, targetFolder );
+			} else {
+				copyFile( curSource, targetFolder );
+			}
+		} );
+	}
+
+	function copyFile(source, target) {
+
+		let targetFile = target;
+
+		//if target is a directory a new file with the same name will be created
+		if ( fs.existsSync( target ) ) {
+			if ( fs.lstatSync( target ).isDirectory() ) {
+				targetFile = path.join( target, path.basename( source ) );
+			}
+		}
+
+		fs.writeFileSync(targetFile, fs.readFileSync(source));
+	}
+	*/
+}
+
+// copyFile('./filesystem/infile.txt', './filesystem/outfile.txt');
+copyFolder('./filesystem/indir', './filesystem/outdir');
